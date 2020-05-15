717c5235acf0d3a93d51d68098dc57bec1173d84
Lenever <onuorah.ikechukwuvictor@gmail.com>
2020-05-15 20:40:26 +0100

    Word search almost complete

--- src/word-search/index.js
+++ src/word-search/index.js
@@ -1,3 +1,64 @@
-function wordSearch(words, word) {}
+function wordSearch(words, word) {
+  let startingPositions = [];
+
+  for (let row = 0; row < words.length; row++) {
+    for (let column = 0; column < words[row].length; column++) {
+      if (words[row][column] === word[0]) {
+        startingPositions.push([row, column]);
+      }
+    }
+  }
+
+  console.log(startingPositions);
+
+  for (let each of startingPositions) {
+    let startingRow = parseInt(each[0]);
+    let startingColumn = parseInt(each[1]);
+
+    let copyOfWords = words;
+
+    let trial = [];
+
+    let stack = word.split("");
+
+    function tuning() {
+      trial.push(copyOfWords[startingRow][startingColumn]);
+      copyOfWords[startingRow][startingColumn] = "-";
+      stack.shift();
+      console.log(copyOfWords);
+    }
+
+    while (stack.length > 0) {
+      tuning();
+
+      let rightColumn =
+        startingColumn + 1 < words[startingRow].length
+          ? startingColumn + 1
+          : startingColumn;
+      let downRow =
+        startingRow + 1 < words.length ? startingRow + 1 : startingRow;
+      let leftColumn =
+        startingColumn - 1 > 0 ? startingColumn - 1 : startingColumn;
+      let upRow = startingRow - 1 > 0 ? startingRow - 1 : startingRow;
+
+      if (copyOfWords[startingRow][rightColumn] === stack[0]) {
+        startingColumn = startingColumn + 1;
+      } else if (copyOfWords[downRow][startingColumn] === stack[0]) {
+        startingRow = startingRow + 1;
+      } else if (copyOfWords[startingRow][leftColumn] === stack[0]) {
+        startingColumn = startingColumn - 1;
+      } else if (copyOfWords[upRow][startingColumn] === stack[0]) {
+        startingRow = startingRow - 1;
+      } else {
+        break;
+      }
+    }
+
+    if (trial.join("") === word) {
+      return true;
+    }
+  }
+  return false;
+}
 
 module.exports = wordSearch;
